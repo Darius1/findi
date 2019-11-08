7ef7a833bf883b65b506dbdfbbe66538a30ac278
Darius McFarland <Darius1@users.noreply.github.com>
2019-11-08 00:13:07 -0500

    Update findi_list.py
    Fixed an issue where an error in opening or loading an info.txt file for an IP would cause the script to stop running. This prevented all following items in the openIPs folder from being printed out.
    
    Cleaned up the formatting in the document and the print statements
    
    This commit addresses issue #1

--- Findi/findi_list.py
+++ Findi/findi_list.py
@@ -9,19 +9,29 @@ listOfEntries = listdir(win_path)
 filesSizeZero = 0
 listOfZero = ""
 
-#print(listOfEntries)
 for each in listOfEntries:
-        open_file_str = ".\\openIPs\\" + each + "\\info.txt"
+    open_file_str = ".\\openIPs\\" + each + "\\info.txt"
+
+    try:
         pageDataFile = open(open_file_str, "rb")
         pageData = pickle.load(pageDataFile)
         pageDataFile.close()
-        if pageData["size"]>0:
-                try:
-                        print(each+"----"+" http://"+pageData["address"]+" ---"+pageData["title"]+"---("+str(pageData["size"])+"B)---"+pageData["comment"])
-                except:
-                        print(each+"----"+" http://"+pageData["address"]+"  READ ERROR")
+
+        if pageData["size"] > 0:
+            try:
+                print(each + " ----" + " http://" + pageData["address"] + " --- " + pageData["title"] + " ---(" + str(
+                    pageData["size"]) + "B)---" + pageData["comment"])
+            except:
+                print(each + " ----" + " http://" +
+                      pageData["address"] + " READ ERROR")
         else:
-                listOfZero += each+"---- http://"+pageData["address"] + "\n"
-                filesSizeZero = filesSizeZero+1
-print("\nFiles with size zero: "+str(filesSizeZero))
+            listOfZero += each + " ---- http://" + pageData["address"] + "\n"
+            filesSizeZero = filesSizeZero + 1
+
+    except Exception as e:
+        print("\nUnable to open file: " +
+              open_file_str + "due to error: " + repr(e) + "\n")
+
+print("\nFiles with size zero: " + str(filesSizeZero))
+print("-----------------------------------------------")
 print(listOfZero)
