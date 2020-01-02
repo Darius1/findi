be29e222de4f4dfd0343c506ee9ba9548139d1bd
Darius McFarland <Darius1@users.noreply.github.com>
2020-01-01 21:06:02 -0500

    Implemented webpage parsing functionality
    Will be able to process pages for their title, and determine what type of device they represent and if they are password protected.
    
    This commit addresses issue #13

--- Findi/page_parser.py
+++ Findi/page_parser.py
@@ -1,30 +1,48 @@
-def print_results(rendered_page):
-    print(f"Size of webpage: {len(rendered_page.html)} bytes")
-    # print(rendered_page)
+import datetime
+
+page_data = {}
+
+def print_results():
+    print(page_data)
+
+def get_scan_results():
+    return page_data
 
 def gather_page_metadata(rendered_page):
-    check_page_title(rendered_page)
-    check_for_login(rendered_page)
-    check_for_router(rendered_page)
-    check_for_camera(rendered_page)
-    check_for_storage(rendered_page)
+    page_data["Scan Date"] = f"{datetime.datetime.now():%m-%d-%Y %H:%M}"
+    page_data["Page Title"] = check_page_title(rendered_page)
+    page_data["Login Found"] = check_for_login(rendered_page)
 
+    comments = ""
+    comments += check_for_router(rendered_page)
+    comments += check_for_camera(rendered_page)
+    comments += check_for_storage(rendered_page)
+    page_data["Comments"] = comments
+
+    page_data["Page Size"] = f"{len(rendered_page.html)} bytes"
+
+    print_results()
+    
 def check_page_title(rendered_page):
     title = rendered_page.find("title", first=True)
-    print(title.text)
+    return title.text
 
 def check_for_login(rendered_page):
     if rendered_page.search("password"):
-        print("login found")
+        return True
+    return False
 
 def check_for_router(rendered_page):
     if rendered_page.search("router") or rendered_page.search("access point"):
-        print("router/access point found")
+        return "Router/Access Point found\n"
+    return ""
 
 def check_for_camera(rendered_page):
     if rendered_page.search("camera"):
-        print("camera found")
+        return "Camera found\n"
+    return ""
 
 def check_for_storage(rendered_page):
     if rendered_page.search("storage"):
-        print("online storage found")
+        return "Online storage found\n"
+    return ""
--- Findi/page_renderer.py
+++ Findi/page_renderer.py
@@ -1,10 +1,10 @@
 from requests_html import HTMLSession
+import page_parser as parser
 
 def load_page(website):
     # create an HTML Session object
     session = HTMLSession()
     
-    # website = "http://71.45.49.173/"
     # Use the object above to connect to needed webpage
     resp = session.get(website)
     
@@ -19,4 +19,5 @@ def load_page(website):
     return rendered_page
 
 if __name__ == "__main__":
-    load_page("http://71.45.49.173/")
+    p = load_page("http://71.45.49.173/")
+    parser.gather_page_metadata(p)
